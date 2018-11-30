33ff080ea914020f82092fd22ebdc5ffda2728ae
smjenness <smjenness@gmail.com>
2018-11-30 13:53:52 -0500

    Reorg hyak files

--- hyak/scp.sh
+++ hyak/scp.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+scp *.[Rs]* hyak:/suppscr/csde/sjenness/netparam
+scp data/*.* hyak:/suppscr/csde/sjenness/netparam/data
+scp hyak:/suppscr/csde/sjenness/netparam/data/*.NetEst.*.rda data/
+scp hyak:/suppscr/csde/sjenness/netparam/data/*.NetSim.*.rda data/
+
+shell
+gsr
+cd netparam
+lspack
+module load gcc-8.1.0-gcc-4.4.7-eaajvcy
+module load r-3.5.0-gcc-8.1.0-bcqjjkd
+R
+
 scp data/*.sim1.* hyak:/suppscr/csde/sjenness/netparam/data
 
 scp *.TsnaHyak.R hyak:/suppscr/csde/sjenness/netparam
--- hyak/scripts.sh
+++ hyak/scripts.sh
@@ -1,16 +1,5 @@
+#!/bin/bash
 
-scp *.[Rs]* hyak:/suppscr/csde/sjenness/netparam
-scp data/*.* hyak:/suppscr/csde/sjenness/netparam/data
-scp hyak:/suppscr/csde/sjenness/netparam/data/*.NetEst.*.rda data/
-scp hyak:/suppscr/csde/sjenness/netparam/data/*.NetSim.*.rda data/
-
-shell
-gsr
-cd netparam
-lspack
-module load gcc-8.1.0-gcc-4.4.7-eaajvcy
-module load r-3.5.0-gcc-8.1.0-bcqjjkd
-R
-
-sbatch -p csde -A csde --export=CITY=A runsim.sh
-sbatch -p csde -A csde --export=CITY=S runsim.sh
+## network simulation
+sbatch -p csde -A csde --export=CITY=A runsim.netsim.sh
+sbatch -p csde -A csde --export=CITY=S runsim.netsim.sh
