c8374a61a33c3f56768932113ad616255b95f39c
Emeli J Anderson <emeli.anderson@emory.edu>
2020-10-14 19:44:10 -0400

    Format Diagnostics code

--- Network model/04.Diagnostics.R
+++ Network model/04.Diagnostics.R
@@ -1,24 +1,18 @@
 
 ##
 ## Network modeling diagnostics for ART-Net Data
-## v1: 2018-08
 ##
 
 ## Packages ##
 rm(list = ls())
 suppressMessages(library("EpiModelHIV"))
 
-
 ## Inputs ##
 city_name <- "Atlanta"
-
 
 ## Load Data ##
 fn <- paste("data/artnet.NetEst", gsub(" ", "", city_name), "rda", sep = ".")
 est <- readRDS(file = fn)
-
-# fn <- paste("data/artnet.NetStats", gsub(" ", "", city_name), "rda", sep = ".")
-# nstats <- readRDS(file = fn)
 
 
 # Main --------------------------------------------------------------------
@@ -35,6 +29,7 @@ model_main_dx <- ~edges +
   concurrent +
   nodematch("role.class", diff = TRUE, keep = 1:2) +
   degree(0:3)
+
 dx_main <- netdx(fit_main, nsims = 20, ncores = 8, nsteps = 500,
                  nwstats.formula = model_main_dx,
                  set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
@@ -55,11 +50,11 @@ model_casl_dx <- ~edges +
   concurrent +
   nodematch("role.class", diff = TRUE, keep = 1:2) +
   degree(0:4)
+
 dx_casl <- netdx(fit_casl, nsims = 20, ncores = 8, nsteps = 500,
                  nwstats.formula = model_casl_dx,
                  set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
 print(dx_casl)
-plot(dx_casl)
 
 dx_casl <- netdx(fit_casl, nsims = 5000, dynamic = FALSE,
                  nwstats.formula = model_casl_dx,
@@ -80,6 +75,7 @@ model_inst_dx <- ~edges +
   nodefactor("risk.grp", base = 0) +
   nodematch("role.class", diff = TRUE, keep = 1:2) +
   degree(0:4)
+
 dx_inst <- netdx(fit_inst, nsims = 5000, dynamic = FALSE,
                  nwstats.formula = model_inst_dx,
                  set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
